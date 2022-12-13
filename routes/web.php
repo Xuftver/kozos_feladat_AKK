72e34a0acbde539306569c8824015b929c1a42b1
Xu212 <matexu97@gmail.com>
2022-12-13 11:59:58 +0100

    UserController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/api/users', [UserController::class,'index']);
+Route::get('/api/users/{id}', [UserController::class,'show']);
+Route::post('/api/users', [UserController::class,'store']);
+Route::put('/api/users/{id}', [UserController::class,'update']);
+Route::patch('/api/users/{id}', [UserController::class,'update']);
+Route::delete('/api/users/{id}', [UserController::class,'destroy']);
