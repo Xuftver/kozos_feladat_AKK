391165187ba774ce727c5f304e03e14f59b07cab
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-12-19 00:10:21 +0100

    AdatBeolvasmodell (get, post, put, delete) aszinkron hívásai

--- public/js/model/AdatbeolvasModel.js
+++ public/js/model/AdatbeolvasModel.js
@@ -36,6 +36,80 @@ class AdatBeolvasModell
                 console.error('Error:', error);
             });
     }
+
+    adatUj(vegpont, adat)
+    {
+        fetch(vegpont,
+        {
+            method: "POST",
+            headers:
+            {
+                "content-type": "application/json",
+                "X-CSRF-TOKEN": this.token,
+            },
+            body: JSON.stringify(adat),
+        })
+        .then((response)=>response.json())
+        .then((data)=>
+        {
+            console.log("Sikeres adatfelvitel" + data);
+        })
+        .catch((error)=>
+        {
+            console.error("Error", error);
+        });
+    }
+
+    adatModosit(vegpont, adat)
+    {
+        console.log(adat);
+        console.log("módosít", vegpont);
+        vegpont+="/" + adat.id;
+        fetch(vegpont,
+        {
+            method: "PUT",
+            headers:
+            {
+                "content-type": "application/json",
+                "X-CSRF-TOKEN": this.token,
+            },
+            body: JSON.stringify(adat),
+        })
+        .then((response)=>response.json())
+        .then((data) => 
+        {
+            console.log("Sikeres módosítás" + data.updatedAt);
+        })
+        .catch((error) => 
+        {
+            console.error('Error:', error);
+        });
+    }
+
+    adatTorol(vegpont, adat)
+    {
+        console.log("töröltem: ");
+        console.log(adat);
+        vegpont +="/"+adat.id;
+        console.log(vegpont);
+        fetch(vegpont,
+        {
+            method: "DELETE",
+            headers:
+            {
+                "X-CSRF-TOKEN": this.token,
+            },
+        })
+        .then((response)=>response.json())
+        .then(() => 
+        {
+            console.log("Sikeres törlés");
+        })
+        .catch((error) => 
+        {
+            console.error('Error:', error);
+        });
+    }
 }
 
 export default AdatBeolvasModell;
