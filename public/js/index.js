97c60da1b1a8d7035e2b43c39a7045ce69a8f6cd
WeinbergerPeti <weinbergerpeti@gmail.com>
2023-01-26 00:33:06 +0100

    előző verzióra visszaállítás

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
-import EsemenyPublikusController from "./controller/EsemenyPublikusController.js";
+// import PublikusEsemenyek from "./utvonal/PublikusEsemenyek.js";
 import FooterKinezet from "./designe/FooterKinezet.js";
 
 $(function()
 {
     console.log("index.js");
-    new EsemenyPublikusController();
+    // new PublikusEsemenyek();
     new FooterKinezet();
 });
